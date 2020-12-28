25540a769edecb2a78a47ba11af56960f682481e
Delyan Milchev <d_milchev99@abv.bg>
2020-12-28 16:42:07 +0200

    Add task Generic Box of Integer

---
+++ Java Advanced - Exercises/Generics - Exercises/GenericBoxOfInteger_02/src/Box.java
@@ -0,0 +1,12 @@
+public class Box <T>{
+    private T value;
+
+    public Box(T value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString(){
+        return this.value.getClass().getName() + ": " + this.value;
+    }
+}
