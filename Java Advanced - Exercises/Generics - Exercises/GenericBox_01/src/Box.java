19ea3a2813b7ce2fe9d82e695bc938799e2b47c1
Delyan Milchev <d_milchev99@abv.bg>
2020-12-28 16:23:28 +0200

    Add task Generic Box

---
+++ Java Advanced - Exercises/Generics - Exercises/GenericBox_01/src/Box.java
@@ -0,0 +1,11 @@
+public class Box <T>{
+    private T value;
+    public Box(T value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString(){
+        return (value.getClass().getName() + ": " + value);
+    }
+}
