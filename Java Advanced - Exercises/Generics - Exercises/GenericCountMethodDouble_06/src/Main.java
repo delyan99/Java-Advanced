99a4ba87e69b65fc5a18bf409fbedd2122a93c56
Delyan Milchev <d_milchev99@abv.bg>
2020-12-28 17:48:43 +0200

    Add task Generic Count Method Double

---
+++ Java Advanced - Exercises/Generics - Exercises/GenericCountMethodDouble_06/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<Double> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            double input = Double.parseDouble(scanner.nextLine());
+            box.add(input);
+        }
+        double elementForCompere = Double.parseDouble(scanner.nextLine());
+        int result = box.compareTo(elementForCompere);
+        System.out.println(result);
+
+    }
+}
