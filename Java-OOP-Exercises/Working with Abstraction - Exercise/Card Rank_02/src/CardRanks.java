ad4402a76283919b85188cd1ac0f9556df901036
Delyan Milchev <d_milchev99@abv.bg>
2021-01-15 12:43:28 +0200

    Add task Card Rank

---
+++ Java-OOP-Exercises/Working with Abstraction - Exercise/Card Rank_02/src/CardRanks.java
@@ -0,0 +1,15 @@
+public enum CardRanks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
---
+++ Java-OOP-Exercises/Working with Abstraction - Exercise/Card Rank_02/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input + ":");
+        for (CardRanks card : CardRanks.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", card.ordinal(), card.name());
+        }
+    }
+}
