9db4b3b8324dd3e2cb5e9e883d960d6d20fd0362
Delyan Milchev <d_milchev99@abv.bg>
2021-01-25 12:39:23 +0200

    Add task Say Hello Extended

---
+++ Java-OOP-Exercises/Interfaces and Abstraction - Lab/Say Hello Extended_04/src/Bulgarian.java
@@ -0,0 +1,16 @@
+public class Bulgarian extends BasePerson{
+
+    public Bulgarian(String name){
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
---
+++ Java-OOP-Exercises/Interfaces and Abstraction - Lab/Say Hello Extended_04/src/Chinese.java
@@ -0,0 +1,17 @@
+public class Chinese extends BasePerson{
+
+    public Chinese(String name){
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+
+}
