1b2960f6f5f99414593b7d96f53d525b2dbdbe4b
Delyan Milchev <d_milchev99@abv.bg>
2021-01-29 22:06:40 +0200

    Add task Animals

---
+++ Java-OOP-Exercises/Polymorphism - Lab/Animals_03/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal{
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("%s%nMEEOW", super.explainSelf());
+    }
+}
---
+++ Java-OOP-Exercises/Polymorphism - Lab/Animals_03/src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal{
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("%sDJAAF", super.explainSelf());
+    }
+}
